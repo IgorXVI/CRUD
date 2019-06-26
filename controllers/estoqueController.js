6514af2b7963a4fdbe83c2e9c8523effc76675e3
Igor Almeida <inazumaseleven04@gmail.com>
2019-06-25 21:21:35 -0300

    Delete estoqueController.js

--- controllers/estoqueController.js
+++
@@ -1,9 +0,0 @@
-const Controller = require("./Controller")
-
-module.exports = class EstoqueController extends Controller{
-    constructor(){
-        super(`estoque`, `produto-estocado`, `quantidade, produto, dataAlteracao, dataCriacao`, true)
-
-        this.masterDAO = this.estoqueDAO
-    }
-}
