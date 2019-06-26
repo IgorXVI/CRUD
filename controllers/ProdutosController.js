e6cc2ac824b98e1b4cd55789605d8b52ea0be112
Igor Almeida <inazumaseleven04@gmail.com>
2019-06-25 21:26:13 -0300

    arrumando bug do github que nao reconhece uppercase

---
+++ controllers/ProdutosController.js
@@ -0,0 +1,31 @@
+const Controller = require("./Controller")
+
+module.exports = class ProdutosController extends Controller {
+    constructor() {
+        super(`produtos`, `produto`, `nome, categoria, precoUnidade, fornecedor, dataAlteracao, 
+        dataCriacao, descricao, garantia, dataFabric, dataValidade`, false)
+
+        this.masterDAO = this.produtosDAO
+
+        super.gerarRotaBuscaTodos()
+        super.gerarRotaBuscaUm()
+        super.gerarRotaAdicionaUm(this.validacaoCustomizada(true))
+        super.gerarRotaAtualizaUm(this.validacaoCustomizada(false))
+        super.gerarRotaDeletaUm()
+    }
+
+    validacaoCustomizada(obrigatorio) {
+        let validacao = super.gerarValidacao(obrigatorio)
+
+        validacao.push(this.body("nome").custom(nome => {
+            return this.produtosDAO.buscaPorNome(nome).then(produto => {
+                if (produto) {
+                    return Promise.reject('O atributo nome informado já está cadastrado.');
+                }
+            });
+        }).optional())
+
+        return validacao
+    }
+
+}
