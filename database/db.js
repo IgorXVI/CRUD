c8099620fb2e83bd4f33c13d426fa2ed3152da14
inazumaseleven04@gmail.com <inazumaseleven04@gmail.com>
2019-08-20 18:24:51 -0300

    atualizando o db.js

--- database/db.js
+++ database/db.js
@@ -1,13 +1,9 @@
 const log = require('log-to-file')
-const fs = require("fs")
 const Database = require('better-sqlite3')
 
 const db = new Database('data.db',  { verbose: log })
 log("Conectado ao banco de dados SQLite data.db")
 console.log("Conectado ao banco de dados SQLite data.db")
-
-// const migration = fs.readFileSync('database/migrate-schema.sql', 'utf8')
-// db.exec(migration)
 
 process.on('exit', () =>{
     db.close()
